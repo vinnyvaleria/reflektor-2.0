f313ec1e57b00caa542cbb87d8d8321727c5b5c5
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-08-23 11:28:31 +0800

    create generate map function with default difficulty set as easy

--- src/lib/server/mapGenerator.js
+++ src/lib/server/mapGenerator.js
@@ -1,1 +1,23 @@
-export class MapGenerator {}
+export class MapGenerator {
+	static generate(difficulty = 'EASY') {
+		const sizes = { EASY: 5, MEDIUM: 7, HARD: 9 };
+		const size = sizes[difficulty];
+
+		const map = this.createEmptyMap(size);
+
+		return {
+			mainMap: map,
+			mirroredMap: map, // placeholder; real mirroring later
+			metadata: {
+				size,
+				difficulty
+			}
+		};
+	}
+
+	static createEmptyMap(size) {
+		return Array(size)
+			.fill(0)
+			.map(() => Array(size).fill(0));
+	}
+}
