d440d58c2dac0bb8cea5bee48ca006710c4675a5
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-08-23 11:52:06 +0800

    created random map generator function

--- src/lib/server/mapGenerator.js
+++ src/lib/server/mapGenerator.js
@@ -3,7 +3,7 @@ export class MapGenerator {
 		const sizes = { EASY: 5, MEDIUM: 7, HARD: 9 };
 		const size = sizes[difficulty];
 
-		const map = this.createEmptyMap(size);
+		const map = this.createRandomMap(size);
 
 		return {
 			mainMap: map,
@@ -20,4 +20,23 @@ export class MapGenerator {
 			.fill(0)
 			.map(() => Array(size).fill(0));
 	}
+
+	static createRandomMap(size) {
+		const map = this.createEmptyMap(size);
+
+		// place player (bottom area - easier to see)
+		const playerRow = Math.floor(size * 0.7) + Math.floor(Math.random() * Math.floor(size * 0.3));
+		const playerCol = Math.floor(Math.random() * Math.floor(size * 0.4));
+		map[playerRow][playerCol] = 2;
+
+		// place goal (top-right area)
+		const goalRow = Math.floor(Math.random() * Math.floor(size * 0.4));
+		const goalCol = Math.floor(size * 0.6) + Math.floor(Math.random() * Math.floor(size * 0.4));
+		map[goalRow][goalCol] = 3;
+
+		return map;
+	}
 }
+
+// console.log(MapGenerator.createEmptyMap(3));
+console.log(MapGenerator.generate('EASY'));
